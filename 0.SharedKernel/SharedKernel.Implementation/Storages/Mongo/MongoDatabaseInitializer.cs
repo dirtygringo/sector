2e22a7a5055b813afd735cd79e2cfc0218446d30
Nenad Mihajlovic <nenad.mihajlovic@namics.com>
2018-08-28 19:04:38 +0200

    Adding Mongo folder to implementation

---
+++ 0.SharedKernel/SharedKernel.Implementation/Storages/Mongo/MongoDatabaseInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NM.SharedKernel.Implementation.Storages.Mongo
+{
+    internal class MongoDatabaseInitializer
+    {
+    }
+}
---
+++ 0.SharedKernel/SharedKernel.Implementation/Storages/Mongo/MongoOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NM.SharedKernel.Implementation.Storages.Mongo
+{
+    internal class MongoOptions
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
